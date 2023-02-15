21822c1180f1dce77c666424abe4cda6c97abc48
AnaPejovska <ana_pejovska@yahoo.com>
2023-02-16 00:33:51 +0100

    Added Prometheus.yaml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,11 +8,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @Configuration
+@RestController
 public class DemoApplication {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Working...!!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
